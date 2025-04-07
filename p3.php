c8caafd5860fa82895d3495b43230a296d72d512
Youssef GARA <youssef.gara@insat.ucar.tn>
2025-04-07 17:26:16 +0100

    sessionPage finished, p3 problem found

--- p3.php
+++ p3.php
@@ -1,12 +1,12 @@
 <?php
 // Exercise 2 
 class Session {
-    private $data = [];
+    private $data = []; 
     private $sessionId;
-    private data["nbVisits"] = 0;
 
     public function __construct($sessionId) {
         $this->sessionId = $sessionId;
+        $this->data["nbVisits"] = 0; 
     }
 
     public function set($key, $value) {
@@ -17,14 +17,26 @@ class Session {
         return isset($this->data[$key]) ? $this->data[$key] : null;
     }
 
-    public function incrementVisits() {
-      if (isset($this->data["nbVisits"])){
-        $this->data["nbVisits"]++;
-      } else {
-        $this->data["nbVisits"] = 1;
-      }
+    public function delete($key) {
+        if (isset($this->data[$key])) {
+            unset($this->data[$key]);
+        }
     }
 
+    public function getSessionId() {
+        return $this->sessionId;
+    }
 
+    public function incrementVisits() {
+        if (isset($this->data["nbVisits"])) {
+            $this->data["nbVisits"]++;
+        } else {
+            $this->data["nbVisits"] = 1;
+        }
+    }
+
+    public function getData() {
+        return $this->data; 
+    }
 }
 ?>
